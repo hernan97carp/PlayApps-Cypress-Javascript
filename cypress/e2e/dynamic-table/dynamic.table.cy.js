702a17465e52887d7a2fb2e1f944e1785e7dc3f9
hernan <hernan97carp@hotmail.com>
2025-03-13 11:49:32 -0300

    Using DOM, created a page called dynamicTablePage.js

--- cypress/e2e/dynamic-table/dynamic.table.cy.js
+++ cypress/e2e/dynamic-table/dynamic.table.cy.js
@@ -1,24 +1,19 @@
-describe('Testing Dynamic table mini application', () => {
-    let heroAlias;
-    let realName;
-  
+import { DynamicTablePage } from "../../support/pages/dynamicTablePage";
+
+describe('Testing Dynamic Table mini application', () => {
+    const dynamicTablePage = new DynamicTablePage();
+    
+    beforeEach(() => {
+        cy.visit(dynamicTablePage.url);
+    });
+
     it('TC:01 Find the username Peter Parker', () => {
-      cy.visit('https://qaplayground.dev/apps/dynamic-table/');
-  
-      // Find "Spider-Man" alias and store it
-      cy.contains('Spider-Man').then(($name) => {
-        heroAlias = $name.text().trim(); // Store the cleaned alias
-        expect(heroAlias).to.equal('Spider-Man'); // Assert that alias is exactly 'Spider-Man'
-      });
-  
-      cy.get('tr')
-        .contains('td', 'Peter Parker') // Find <td> element that contains 'Peter Parker'
-        .parents('tr') // Get the <tr> row that contains 'Peter Parker'
-        .find('td:last-child') // Select the last <td> element within that row
-        .invoke('text') // Extract the text from the <td>
-        .then(($text) => {
-          realName = $text.trim(); // Store the cleaned real name
-          expect(realName).to.equal('Peter Parker'); // Assert that real name is 'Peter Parker'
+        dynamicTablePage.getHeroAliasSpiderMan().then((alias) => {
+            expect(alias).to.equal(dynamicTablePage.spiderManHero.alias); 
+        });
+
+        dynamicTablePage.getNameHeroSpiderMan().then((name) => {
+            expect(name).to.equal(dynamicTablePage.spiderManHero.name);
         });
     });
-  });
+});
